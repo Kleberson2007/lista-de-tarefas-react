c65009179924795d886a33420956adc9bd601439
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-07-15 13:03:45 -0300

    s

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './style.css'
-import './style.css'
 import React from 'react';
+
+const tarefas = ["estudar HTML", "estudar CSS", "estudar JS"]
 
 export default function App() {
   return (
-    <div>
-      <h1>lista de tarefas</h1>
-      
+    <div className='listadetarefas'>
+      <header>
+        <h1>Lista de tarefas</h1>
+      </header>
+      <main>
+        <ul>{tarefas}</ul>        
+      </main>
+      <footer>
+        <input></input>
+        <button onClick="add">adicionar</button>
+      </footer>
     </div>
   );
 }
