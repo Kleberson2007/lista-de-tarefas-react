634e927fd8bb683c35543396f7357d98f2675578
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-07-16 16:54:15 -0300

    :D

--- src/App.js
+++ src/App.js
@@ -14,15 +14,47 @@ export default function App() {
     setNova("")
   }
 
+  function deletar(i) {
+    
+  }
+  
+  function subir(i) {
+    
+  }
+
+  function descer(i) {
+    
+  }
+
+  function enter(event) {
+    if (event.key === 'Enter') {
+      adicionar();
+    }
+  }
+
   return (
     <div className='listadetarefas'>
       <header>
         <h1>Lista de tarefas</h1>
       </header>
-      <input value={nova} onChange={e => setNova(e.target.value)} />
-      <button onClick={adicionar}>add</button>
+      <div>
+        <input
+          type='text'
+          placeholder='nome da tarefa...'
+          value={nova}
+          onChange={e => setNova(e.target.value)}
+          onKeyDown={enter} />
+
+        <button onClick={adicionar}>
+          add
+        </button>
+      </div>
       <ul>
-        {tarefas.map((t, i)=> <li key={i}>{t}</li>)}
+        {tarefas.map((t, i)=> 
+          <li key={i}>
+            {t}
+            <button className='botaodeletar' onClick={deletar(i)}>deletar</button>
+          </li>)}
       </ul>
     </div>
   );
